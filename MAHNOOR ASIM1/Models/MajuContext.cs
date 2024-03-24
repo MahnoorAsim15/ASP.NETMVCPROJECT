fc9448546bb84778ea50c9b1ab5349ee40128019
Muhammad Asharib <164147267+MahnoorAsim15@users.noreply.github.com>
2024-03-24 05:24:34 +0500

    Initial commit

---
+++ MAHNOOR ASIM1/Models/MajuContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MAHNOOR_ASIM1.Models;
+
+public partial class MajuContext : DbContext
+{
+    public MajuContext()
+    {
+    }
+
+    public MajuContext(DbContextOptions<MajuContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-NRTFR44\\SQLEXPRESS;Database=MAJU;Trusted_Connection=True;trustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC0732B9E696");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Age).HasColumnName("AGE");
+            entity.Property(e => e.Contact)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("CONTACT");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("NAME");
+            entity.Property(e => e.Semester)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("SEMESTER");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MAHNOOR ASIM1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MAHNOOR_ASIM1.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Semester { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Contact { get; set; }
+}
